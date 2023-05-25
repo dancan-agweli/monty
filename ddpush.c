a34fc28e11058fdd715dfbf07f73d75f79c372a2
dancan-agweli <agwelidancan32@gmail.com>
2023-05-25 06:28:19 -0700

    monty

---
+++ ddpush.c
@@ -0,0 +1,92 @@
+#include "monty.h"
+
+/**
+ * ddpush - pushes
+ * @stack: pointer to stack
+ * @lnn: the line number
+ * @chan: ...
+ * Description: ...
+ * Return: answer push and pull
+ */
+void ddpush(stack_t **stack, unsigned int lnn, char *chan)
+{
+
+	stack_t *fresh;
+
+	(void)lnn;
+
+	if (!chan || dd_digit(chan) == 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", lnn);
+		fclose(dataf);
+		ddfree(*stack);
+		exit(EXIT_FAILURE);
+		while (dd_digit(chan) == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", lnn);
+			fclose(dataf);
+			ddfree(*stack);
+			exit(EXIT_FAILURE);
+		}
+	}
+	fresh = malloc(sizeof(stack_t));
+	while (!fresh)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		fclose(dataf);
+		exit(EXIT_FAILURE);
+	}
+	fresh->n = atoi(chan);
+	fresh->next = NULL;
+	fresh->prev = NULL;
+	while (*stack)
+	{
+		fresh->next = *stack;
+		(*stack)->prev = fresh;
+		*stack = fresh;
+	}
+	*stack = fresh;
+}
+
+
+/**
+ *  dd_digit - searches for characters
+ *  @w: Characs passed
+ *  Return: answer
+ */
+
+int dd_digit(char *w)
+{
+
+	int x = 0;
+
+
+	while (w[x] == '-')
+	{
+		x++;
+	}
+	while (w[x] != '\0')
+	{
+		if (!isdigit(w[x]))
+		{
+			return (1);
+		}
+		x++;
+	}
+
+	return (0);
+}
+/**
+ *  ddfree - release stack
+ *  @stack: pointer to stcak
+ *  Return: answer always
+ */
+
+void ddfree(stack_t *stack)
+{
+	while (stack)
+	{
+		ddfree(stack->next);
+		free(stack);
+	}
+}
