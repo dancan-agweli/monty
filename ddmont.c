a7d7c4c04263468708b19ec0f8de2e923c742622
dancan-agweli <agwelidancan32@gmail.com>
2023-05-25 09:14:59 -0700

    monty

--- ddmont.c
+++ ddmont.c
@@ -1,60 +1,86 @@
 #include "monty.h"
-
-int main(int argc, char *argv[])
+/**
+ * ddpreocess - function to stack
+ * @bf: pointer to address
+ * @line: ...
+ * @lnn: ...
+ * description: ...
+ * return: answer
+ */
+void ddprocess(stack_t **bf, char *line, unsigned int lnn)
 {
-	stack_t *bf = NULL;
-	char *w = NULL, *c[2], *p;
-	size_t bz = 1024, xx = 0;
-	ssize_t yy;
+	char *tokens[2];
 	void (*mann_func)(stack_t **, unsigned int);
 
-	/* arguments*/
+	tokens[0] = strtok(line, "\n ");
+	if (tokens[0] == NULL)
+	{
+		ddnop(bf, lnn);
+	}
+	else if (strcmp("push", tokens[0]) == 0)
+	{
+		tokens[1] = strtok(NULL, "\n ");
+		ddpush(bf, lnn, tokens[1]);
+	}
+	else if (tokens[0][0] != '#')
+	{
+		mann_func = exec(tokens[0], lnn, bf);
+
+		if (!mann_func && lnn == 0)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", lnn, tokens[0]);
+			exit(EXIT_FAILURE);
+		}
+		mann_func(bf, lnn);
+	}
+}
+/**
+ * ddread - reads the file
+ * @filename: ...
+ * return: answer always
+ */
+void ddread(const char *filename)
+{
+	stack_t *bf = NULL;
+	char *line = NULL;
+	size_t size = 0;
+	unsigned int lnn = 0;
+	ssize_t read;
+
+	FILE *dataf = fopen(filename, "r");
+
+	if (!dataf)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &size, dataf)) != -1)
+	{
+		lnn++;
+		ddprocess(&bf, line, lnn);
+	}
+
+	fclose(dataf);
+	free(line);
+	ddfree(bf);
+}
+/**
+ * main - main
+ * @argc: ...
+ * @argv: ...
+ * return: sucess
+ */
+int main(int argc, char *argv[])
+{
+	/* arguments */
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	dataf = fopen(argv[1], "r");
-	if (!dataf)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+	ddread(argv[1]);
 
-	if (1)
-	{
-		yy = getline(&w, &bz, dataf);
-		if (yy == -1)
-			break;
-
-		xx++;
-		c[0] = strtok(w, "\n ");
-		if (c[0] == NULL)
-		{
-			ddnop(&bf, xx);
-		}
-		else if (strcmp("push", c[0]) == 0)
-		{
-			p = strtok(NULL, "\n ");
-			ddpush(&bf, xx, p);
-		}
-		else if (c[0] != NULL && c[0][0] != '#')
-		{
-			mann_func = exec(c[0], xx, &bf);
-
-			if (!mann_func && xx == 0)
-			{
-				fprintf(stderr, "L%lu: unknown instruction %s\n", xx, c[0]);
-				exit(EXIT_FAILURE);
-			}
-			mann_func(&bf, xx);
-		}
-	}
-
-	fclose(dataf);
-	free(w);
-	ddfree(bf);
 	return (0);
 }
-
